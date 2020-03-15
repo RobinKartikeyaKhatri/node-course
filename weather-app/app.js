f0a2959b8082203a9e0bf9e65af8f83e58851ede
Robin Kartikeya Khatri <kartikrobin@gmail.com>
2020-03-15 09:15:01 +0530

    Learn about MapBox API and completed a little challenge based on it

--- weather-app/app.js
+++ weather-app/app.js
@@ -6,9 +6,17 @@ request({ url: url, json: true }, (error, response) => {
     console.log(`${response.body.daily.data[0].summary} It is currently ${response.body.currently.temperature} degrees out. There is a ${response.body.currently.precipProbability}% chance of rain.`);
 });
 
+// Goal: Print the lat/long for Los Angeles
+// 
+// 1. Fire off a new request to the URL explored in browser
+// 2. Have the request module parse it as json
+// 3. Print both the latitude and longitude to the terminal
+// 4. Test your work
 
-// 
-// Goal: Print a small forecast to the user
-// 
-// 1. Print: "It is currently 58.55 degrees out. There is a 0% chance of rain."
-// 2. Test your work
+const geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1Ijoicm9iaW5rYXJ0aWtleWEiLCJhIjoiY2s3c2Z2OG1oMGNxbjNycGdzbnAyYzZ6ZSJ9.mnuYyiqslJo0Fx4nh_EbEw&limit=1';
+
+request({ url: geocodeURL, json: true}, (error, response) => {
+    const latitude = response.body.features[0].center[1];
+    const longitude = response.body.features[0].center[0];
+    console.log(latitude, longitude);
+});
