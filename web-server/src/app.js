b0369f55c5785767fa87b7ffb9d0e56d3f395e21
Robin Kartikeya Khatri <kartikrobin@gmail.com>
2020-03-20 17:54:41 +0530

    Learn how to send HTML code, JSON with the help of send() method

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -3,26 +3,41 @@ const express = require("express");
 const app = express();
 
 app.get("", (req, res) => {
-    res.send("Hello Express!");
+    res.send("<h1>Weather</h1>");
 });
 
 app.get("/help", (req, res) => {
-    res.send("Help page");
+    res.send([{
+        name: "Andrew Mead",
+        age: 27
+    },{
+        name: "Robin",
+        age: 34
+    },{
+        name: "Lalit",
+        age: 37
+    }]);
 });
 
 // 
-// Goal: Setup two new routes
+// Goal: Update routes
 // 
-// 1. Setup an about route and render a page title
-// 2. Setup a weather route and render a page title
+// 1. Setup about route to render a little with HTML
+// 2. Setup a weather route to send back JSON
+//      - Object with forecast and location strings
 // 3. Test your work by visiting both in the browser
 
+
+
 app.get("/about", (req, res) => {
-    res.send("About page");
+    res.send("<h2>This is from About Route</h2>");
 });
 
 app.get("/weather", (req, res) => {
-    res.send("Weather page");
+    res.send({
+        forecast: "It is hot out there",
+        location: "Barmer, Barmer. Rajasthan"
+    });
 });
 
 app.listen(3000, () => {
