8ca88716b72f636871f4184a97ce480da6e1d943
Robin Kartikeya Khatri <kartikrobin@gmail.com>
2020-04-01 08:04:09 +0530

    Learn how to fetch/read single/multiple data from database using findOne() and find() methods

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -24,40 +24,43 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
 
     const db = client.db(databaseName);
 
-    // db.collection("users").insertOne({
-    //     name: "Vikram",
-    //     age: 26
-    // }, (error, result) => {
+    // db.collection("users").findOne({_id: new ObjectID("5e834b8b9463681d50e89ad8")}, (error, user) => {
     //     if (error) {
-    //         return console.log("Unable to insert user");
+    //         return console.log("Unable to fetch!");
     //     }
 
-    //     console.log(result.ops)
-    // });
-
-    // db.collection("users").insertMany([
-    //     {name: "Jen", age: 28},
-    //     {name: "Gunther", age: 27}
-    // ], (error, result) => {
-    //     if (error) {
-    //         return console.log("Unable to insert users!");
-    //     }
-
-    //     console.log(result.ops);
-    // });
-
-    // db.collection("tasks").insertMany([
-    //     {description: "Clean the house", completed: true},
-    //     {description: "Renew inspection", completed: false},
-    //     {description: "Pot plants", completed: false}
-    // ], (error, result) => {
-    //     if (error) {
-    //         return console.log("Unable to insert tasks");
-    //     }
-
-    //     console.log(result.ops);
+    //     console.log(user);
     // });
 
 
 
+    // db.collection("users").find({age: 34}).toArray((error, users) => {
+    //     console.log(users);
+    // });
+
+
+    // db.collection("users").find({age: 34}).count((error, count) => {
+    //     console.log(count);
+    // });
+
+
+    // 
+    // Goal: Use find and findOne with tasks
+    // 
+    // 1. Use findOne to fetch the last task by its id (print doc to console)
+    // 2. Use find to fetch all tasks that are not completed (print docs to console)
+    // 3. test your work!
+
+
+    db.collection("tasks").findOne({_id: new ObjectID("5e83428fb2c7a7168cef0be6")}, (error, task) => {
+        if (error) {
+            return console.log("Unable to fetch!");
+        }
+
+        console.log(task);
+    });
+
+    db.collection("tasks").find({completed: false}).toArray((error, tasks) => {
+        console.log(tasks);
+    });
 });
