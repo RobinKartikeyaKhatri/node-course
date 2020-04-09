f380aa08cb6f42579338877a7f1e899e502641f9
Robin Kartikeya Khatri <kartikrobin@gmail.com>
2020-04-09 16:39:11 +0530

    Learn how to convert a Promise based function into Async-Await based function, and complete a little challenge based on the same.

--- task-manager/playground/promise-chaining.js
+++ task-manager/playground/promise-chaining.js
@@ -3,11 +3,23 @@ const User = require("../src/models/user");
 
 
 
-User.findByIdAndUpdate("5e8b0b8ddb95a2226846fb81", {age: 1}).then((user) => {
-    console.log(user);
-    return User.countDocuments({age: 1})
-}).then((result) => {
-    console.log(result);
-}).catch((error) => {
-    console.log(error);
+// User.findByIdAndUpdate("5e8b0b8ddb95a2226846fb81", {age: 1}).then((user) => {
+//     console.log(user);
+//     return User.countDocuments({age: 1})
+// }).then((result) => {
+//     console.log(result);
+// }).catch((error) => {
+//     console.log(error);
+// });
+
+const updateAgeAndCount = async (id, age) => {
+    const user = await User.findByIdAndUpdate(id, { age });
+    const count = await User.countDocuments({age});
+    return count;
+}
+
+updateAgeAndCount("5e8b0b8ddb95a2226846fb81", 2).then((count) => {
+    console.log(count);
+}).catch((e) => {
+    console.log(e);
 });
