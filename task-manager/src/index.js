b3eda2251b053f5538c010febbd3dabffce9570f
Robin Kartikeya Khatri <kartikrobin@gmail.com>
2020-04-20 14:23:31 +0530

    Learn about Express middlewares, how to define, and use them.

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -6,6 +6,26 @@ const taskRouter = require("./routers/task");
 const app = express();
 const port = 3000 || process.env.PORT;
 
+// app.use((req, res, next) => {
+//     if (req.method === "GET") {
+//         res.send("GET requests are disabled");
+//     } else {
+//         next();
+//     }
+// });
+
+app.use((req, res, next) => {
+    res.status(503).send("Site is currently down. Check back soon!");
+});
+
+// 
+// Goal: Setup middleware for maintainance mode
+// 
+// 1. Register a new middleware funcion
+// 2. Send back a maintenance message with a 503 status code
+// 3. Try your requests from the server and confirm status/message shows
+
+
 app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
