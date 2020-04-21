475f20d67ad080cf1766649832610a35331593f3
Robin Kartikeya Khatri <kartikrobin@gmail.com>
2020-04-21 18:07:58 +0530

    Learn how to make relationship into mongoose models, and how to populate data from them.

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -26,3 +26,19 @@ app.use(taskRouter);
 app.listen(port, () => {
     console.log("server is up on port " + port);
 });
+
+
+const Task = require("./models/task");
+const User = require("./models/user");
+
+const main = async () => {
+    // const task = await Task.findById("5e9ee03bf653441c24cabad5");
+    // await task.populate("owner").execPopulate()
+    // console.log(task.owner);
+
+    const user = await User.findById("5e9eded2fae1e01a1ce3fec4");
+    await user.populate("tasks").execPopulate()
+    console.log(user.tasks);
+}
+
+main();
