edbaecb17e94414398e60e4bd2b656857a6b67e5
Robin Kartikeya Khatri <kartikrobin@gmail.com>
2020-04-22 16:39:58 +0530

    Learn how to upload images using multer package, and how to configure multer to do the job.

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -6,18 +6,14 @@ const taskRouter = require("./routers/task");
 const app = express();
 const port = 3000 || process.env.PORT;
 
-// app.use((req, res, next) => {
-//     if (req.method === "GET") {
-//         res.send("GET requests are disabled");
-//     } else {
-//         next();
-//     }
-// });
+const multer = require("multer");
+const upload = multer({
+    dest: "images"
+});
 
-// app.use((req, res, next) => {
-//     res.status(503).send("Site is currently down. Check back soon!");
-// });
-
+app.post("/upload", upload.single("upload"), (req, res) => {
+    res.send("")
+});
 
 app.use(express.json());
 app.use(userRouter);
@@ -26,19 +22,3 @@ app.use(taskRouter);
 app.listen(port, () => {
     console.log("server is up on port " + port);
 });
-
-
-const Task = require("./models/task");
-const User = require("./models/user");
-
-const main = async () => {
-    // const task = await Task.findById("5e9ee03bf653441c24cabad5");
-    // await task.populate("owner").execPopulate()
-    // console.log(task.owner);
-
-    const user = await User.findById("5e9eded2fae1e01a1ce3fec4");
-    await user.populate("tasks").execPopulate();
-    console.log(user.tasks);
-}
-
-main();
