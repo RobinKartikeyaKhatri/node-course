11c3108cafd9cdc5f58af4fae6d804a1b6cad386
Robin Kartikeya Khatri <kartikrobin@gmail.com>
2020-05-03 17:11:35 +0530

    Create a input element on front-end to type something in it, grab typed value using socket methods

--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -7,22 +7,18 @@ const app     = express();
 const server  = http.createServer(app);
 const io      = socketio(server); 
 
-
 const port    = process.env.PORT || 3000;
 const publicDirectoryPath = path.join(__dirname, "../public");
 
 app.use(express.static(publicDirectoryPath));
 
-let count = 0;
-
 io.on("connection", (socket) => {
     console.log("New Websocket connection");
 
-    socket.emit("countUpdated", count);
+    socket.emit("message", "Welcome!");
 
-    socket.on("increment", () => {
-        count++;
-        io.emit("countUpdated", count);
+    socket.on("sendMessage", (message) => {
+        io.emit("message", message);
     });
 });
 
